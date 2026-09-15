8f160ac200473e999b5f664c56e639378ce663f7
DoHyuk-Centric <clzlsdlwhgdk12@gmail.com>
2026-09-15 10:02:18 +0900

    feat(api): 캐싱처리 모듈 등록

--- apps/api/src/popular-sectors/popular-sectors.module.ts
+++ apps/api/src/popular-sectors/popular-sectors.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { KrxIndexClient } from './clients/krx-index.client.js';
 import { PopularSectorService } from './popular-sector.service.js';
 import { PopularSectorsController } from './popular-sectors.controller.js';
+import { PopularSectorCache } from './popular-sector.cache.js';
 
 @Module({
   controllers: [PopularSectorsController],
-  providers: [PopularSectorService, KrxIndexClient],
+  providers: [PopularSectorService, PopularSectorCache, KrxIndexClient],
 })
 export class PopularSectorsModule {}
